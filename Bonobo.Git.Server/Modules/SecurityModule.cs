085d74cc16b165e497e44a360522459706619570
Rik Svendsen Rose <rsr@gait.dk>
2018-02-11 16:09:33 +0100

    Add nancy security module

---
+++ Bonobo.Git.Server/Modules/SecurityModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+
+namespace Bonobo.Git.Server.Modules
+{
+    public class SecurityModule : NancyModule
+    {
+        public SecurityModule()
+        {
+            Post["Security/login"] = _ => Test();
+        }
+
+        private static Response Test()
+        {
+
+            return HttpStatusCode.Continue;
+        }
+    }
+}
